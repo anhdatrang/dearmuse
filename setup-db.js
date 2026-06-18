a20bb42a27f296b79144169eeb54ca52885ee655
Nguyễn Văn Hội <nguyenvanhoi.workpc@gmail.com>
2026-06-18 14:17:52 +0700

    sua mat khau db

--- setup-db.js
+++ setup-db.js
@@ -12,7 +12,7 @@ async function setup() {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '123456',
+    password: process.env.DB_PASSWORD || 'admin',
     charset: 'utf8mb4',
   });
 
